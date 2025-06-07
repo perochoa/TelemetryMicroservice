122782328a12682abca2b5b3811a9c8cc5703484
Alexander King Perocho <perocho.xela@gmail.com>
2025-06-07 00:01:26 -0400

    chore: Create Database Model & Context

---
+++ TelemetryMicroservice/Data/VehicleDataContext.cs
@@ -0,0 +1,20 @@
+﻿using TelemetryMicroservice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TelemetryMicroservice.Data
+{
+    public class VehicleDataContext : DbContext
+    {
+        public VehicleDataContext(DbContextOptions<VehicleDataContext> options) : base(options) { }
+        public DbSet<VehicleData> VehicleData { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleData>(entity =>
+            {
+                entity.HasIndex(e => e.VehicleID);
+                entity.HasIndex(e => e.TimeStamp);
+                entity.HasIndex(e => new { e.VehicleID, e.TimeStamp });
+            });
+        }
+    }
+}
---
+++ TelemetryMicroservice/Models/VehicleData.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TelemetryMicroservice.Models
+{
+    [Table("vehicle_data")]
+    public class VehicleData
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public required string VehicleID { get; set; }
+
+        [Column("timestamp")]
+        public DateTime TimeStamp { get; set; }
+
+        // GPS Data
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public double? Altitude { get; set; }
+        public double? GpsSpeed { get; set; }
+        public double? Heading { get; set; }
+        public double? Satellites { get; set; }
+        public double? Hdop { get; set; }
+
+        // OBD Data
+        public double? EngineRpm { get; set; }
+        public double? VehicleSpeed { get; set; }
+        public double? EngineLoad { get; set; }
+        public double? CoolantTemp { get; set; }
+        public double? IntakeAirTemp { get; set; }
+        public double? MafRate { get; set; }
+        public double? ThrottlePosition { get; set; }
+        public double? FuelLevel { get; set; }
+
+        // JSON Column for DTC Codes
+        [Column(TypeName ="jsonb")]
+        public string? DtcCodes { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
